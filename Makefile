db526a805862faa77fe6823063054741c2c0b036
HxX2 <zaka.hanix@gmail.com>
2023-04-08 05:56:33 +0000

    fixed some containers hanging

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 all:
-	@docker_compose -f srcs/requirements/docker-compose.yml up --build
+	@docker-compose -f srcs/docker-compose.yml up --build
 
 stop:
-	@docker_compose -f srcs/requirements/docker-compose.yml down
+	@docker-compose -f srcs/docker-compose.yml down
 
 clean:
 	@docker rm -f $(docker ps -a -q)
 
 fclean:
 	@docker system prune -a -f
-	@docker volume prune -f
+	@docker volume rm -f $(docker volume ls -q) &> /dev/null ; true
 	@docker network prune -f
 
 re: fclean all
--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -2,11 +2,11 @@
 
 service mysql start
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS '$MYSQL_DATABASE';"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
 
 service mysql stop
 
