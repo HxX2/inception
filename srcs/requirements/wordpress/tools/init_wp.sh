a9cb313dc9dccdcb9a2bb1352da9b146329da116
HxX2 <zaka.hanix@gmail.com>
2023-05-09 16:36:32 +0100

    smol fixes

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -9,7 +9,8 @@ sed -i 's/\/run\/php\/php7\.3-fpm\.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.
 
 su www-data -s /bin/bash -c "wp core download --path=/var/www/html"
 su www-data -s /bin/bash -c "wp config create --path=/var/www/html --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST"
-su www-data -s /bin/bash -c "wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL"
+su www-data -s /bin/bash -c "wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL"
+su www-data -s /bin/bash -c "wp user create --path=/var/www/html $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD"
 
 su www-data -s /bin/bash -c "wp plugin install redis-cache --activate --path=/var/www/html"
 
